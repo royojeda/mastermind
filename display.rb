c6aba19a85b2b823b0f3e4af6f26178ac2045e5f
Dennis Ojeda <royojeda1@gmail.com>
2022-05-06 14:20:05 +0800

    Add functionality to handle invalid player role choices

--- display.rb
+++ display.rb
@@ -1,6 +1,5 @@
 module Display
   def display_role_choice
-    system 'clear'
     'Do you want to play as the code maker (1)? or the code breaker (2)?'
   end
 
@@ -18,6 +17,10 @@ module Display
 
   def display_invalid_guess
     'Invalid guess. Please enter numbers from 1 to 6: '
+  end
+
+  def display_invalid_role
+    'Invalid role. Please try again.'
   end
 
   def display_prompt_code
--- main.rb
+++ main.rb
@@ -4,11 +4,29 @@ require_relative 'display'
 def play_mastermind
   include Display
 
-  puts display_role_choice
-  input = gets.chomp.to_i
-  role = input == 1 ? 'maker' : 'breaker'
+  system 'clear'
+  role = prompt_role
   game = Game.new(role)
   game.play
+end
+
+def prompt_role
+  puts display_role_choice
+  input = gets.chomp.to_i
+  validate_role_choice(input)
+end
+
+def validate_role_choice(input)
+  case input
+  when 1
+    'maker'
+  when 2
+    'breaker'
+  else
+    system 'clear'
+    puts display_invalid_role
+    prompt_role
+  end
 end
 
 play_mastermind
