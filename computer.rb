acf599d87913b4fb3a9ba2a639297e814223d187
Dennis Ojeda <royojeda1@gmail.com>
2022-05-07 17:58:43 +0800

    Move the call for list_permutations to initalize

--- computer.rb
+++ computer.rb
@@ -1,23 +1,14 @@
 class Computer
-  attr_reader :nth_guess
-
   def initialize
     @nth_guess = 0
     @step = 0
-  end
-
-  def guess(board)
-    list_permutations
+    @list = list_permutations
   end
 
   def list_permutations
-    list = [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a
+    [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a
+  end
 
-    i = 0
-    while i < list.length
-      p list[i]
-      i += 1
-    end
-    gets
+  def guess(board)
   end
 end
